382c14f2c7e19d269f7ed2620abe4ee132eba6dc
mir77goffer <mir.sn@hotmail.com>
2026-09-15 01:06:51 -0300

    Adicionar arquivo ao projeto.

---
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiAppTempoAgora.Models
+{
+    public class Tempo
+    {
+        public double? lon { get; set; }
+        public double? lat { get; set; }
+
+        public double? temp_min { get; set; }
+        public double? temp_max { get; set; }
+
+        public int? visibility { get; set; }
+        public double? speed { get; set; }
+
+        public string? main { get; set; }
+        public string? description { get; set; }
+
+        public DateTime? sunrise { get; set; }
+        public DateTime? sunset { get; set; }
+    }
+}
